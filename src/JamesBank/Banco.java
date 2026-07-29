c8890f9bdf52d4a23ae1757eb0bed93b2efbdad4
Jamesson Alberto <jamessonalberto9@gmail.com>
2026-07-29 08:27:38 -0300

    Implementado o sistema de cadastro inicial

--- src/JamesBank/Banco.java
+++ src/JamesBank/Banco.java
@@ -1,6 +1,45 @@
 package JamesBank;
 
-public class Banco  {
-	
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Banco  {
+	private String nome;
+	private List <Conta> contas; //lista que guarda todos os Cadastros
+	
+	
+	public Banco(String nome) {
+		this.nome = nome;
+		this.contas = new ArrayList<>();
+	}
+	
+	//metodo para cadastrar a conta na lista do Banco
+	public void adicionarConta(Conta novaConta) {
+		this.contas.add(novaConta);
+		System.out.println(">>>Parabens!!!");
+		System.out.println("Cadastro de "+novaConta.titular.getNome()+ " foi registrado no sistema");
+		
+	}
+	
+	//metodo para buscar conta pelo CPF no usuario
+	public Conta buscarContaCpf(String cpfDigitado) {
+		 for (Conta c : contas) {
+			 
+			 //Verifica se o CPF da conta é igual ao quue o usuario digitou
+			 if(c.getTitular().getCpf().equals(cpfDigitado)) {
+				 return c;
+			 }
+		 }
+		 return null; //Caso nao encontre ninguem com esse CPF
+	}
+	
+	
+	
+	//metodo para ver a quantidade de contas no Sistema
+	public int getTotalContas() {
+		return this.contas.size();
+		
+	}
+	
 }
--- src/JamesBank/Main.java
+++ src/JamesBank/Main.java
@@ -1,77 +1,123 @@
 package JamesBank;
 import java.util.Scanner;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-	
+		Banco jamesBank = new Banco ("Contas JAMES BANK");
+		
 		Scanner sc = new Scanner (System.in);
 		System.out.println("\n=======================");
 		System.out.println("BEM-VINDO AO JAMES BANK!");
 		System.out.println("=======================\n");
 		
-		System.out.print("> Digite seu nome: ");
-		String nome = sc.nextLine();
 		
-		System.out.print("> Digite seu CPF: ");
-		String cpf = sc.nextLine();
+		int opcaoInicial = 0;
 		
-		System.out.print("> Digite seu numero de telefone: ");
-		String telefone = sc.nextLine();
+		//MENU INICIAL DO BANCO 
+		while(opcaoInicial != 3) {
+			System.out.println("\n---- MENU PRINCIPAL ----");
+			System.out.println("[1] - Cadastrar Nova Conta");
+			System.out.println("[2] - Acessar Conta (Login)");
+			System.out.println("[3] - Encerrar sessão...");
+			System.out.println("---------------------------");
+			System.out.println(">>>Escolha sua opção: ");
+			opcaoInicial = sc.nextInt();
+			sc.nextLine(); //Limpar o "Enter" da memoria do teclado
 		
-		//CADASTRO DA CONTA 
-		Cliente cliente1 = new Cliente(nome, cpf, telefone);
-		Conta conta1 = new Conta(1010, cliente1 ); 
-	
-		System.out.println("\n#### Conta de [" +cliente1.getNome()+ "] cadastrada com sucesso!!!\n");
 		
-		int opcao = 0;
-	while (opcao != 5) {
-		System.out.println("\n---- SISTEMA BANCARIO ----");
-		System.out.println("[1] - Depositar");
-		System.out.println("[2] - Sacar");
-		System.out.println("[3] - Extrato da Conta");
-		System.out.println("[4] - Informações da Conta");
-		System.out.println("[5] - Sair da Conta");
-		System.out.println("-----------------------------");
-		System.out.print("Escolha sua opção: ");
-		opcao = sc.nextInt(); //ler a opçao digitada
-		
-		switch (opcao) {
-			case 1:
-				System.out.println("\nSaldo disponível: R$" + conta1.getSaldo());
-				System.out.println("----------------------------");
-				System.out.print("Digite o valor para depósito: R$");
-				double valorDep = sc.nextDouble();
-				conta1.depositar(valorDep);
-				break;
-				
-			case 2:
-				System.out.println("\nSaldo disponível: R$" + conta1.getSaldo());
-				System.out.println("----------------------------");
-				System.out.print("Digite o valor para saque: R$");
-				double valorSaq = sc.nextDouble();
-				conta1.sacar(valorSaq);
-				break;
-				
-			case 3: 
-				conta1.exibirExtrato();
-				break;
-				
-			case 4:
-				conta1.exibirDados();
-				break;
-				
-			case 5:
-				System.out.println("Obrigado por usar o JAMES BANK");
-				System.out.println("Sessão finalizada, até logo...");
-				break;
+			switch (opcaoInicial) {
+				case 1:
 			
-			default:
-				System.out.println("Opção Invalida");
-			}
-	
+					System.out.println("\n--- TELA DE CADASTRO ---"); 
+					System.out.print("> Digite seu nome: ");
+					String nome = sc.nextLine();
+		
+					System.out.print("> Digite seu CPF: ");
+					String cpf = sc.nextLine();
+		
+					System.out.print("> Digite seu numero de telefone: ");
+					String telefone = sc.nextLine();
+				
+					//CADASTRO DA CONTA 
+					Cliente novoCliente = new Cliente(nome, cpf, telefone);
+					Conta contaLogada = new Conta(1010, novoCliente ); 
+			
+					//Salva conta na Lista do Banco
+					jamesBank.adicionarConta(contaLogada);
+					break;
+				case 2:
+					System.out.println("\n--- TELA DE LOGIN ---");
+                    System.out.print(">>> Digite seu CPF para acessar: ");
+                    String buscaCpf = sc.nextLine();
+                    
+                    // O banco procura se o CPF existe na Lista
+                    Conta contaLogada1 = jamesBank.buscarContaCpf(buscaCpf);
+                    
+                    if (contaLogada1 != null) {
+						System.out.println("\n Login realizado com sucesso!");
+						System.out.println("Bem-vido(a), "+contaLogada1.getTitular().getNome());
+						
+						int opcao = 0;
+						while (opcao != 5) {
+							System.out.println("\n---- SISTEMA BANCARIO ----");
+							System.out.println("[1] - Depositar");
+							System.out.println("[2] - Sacar");
+							System.out.println("[3] - Extrato da Conta");
+							System.out.println("[4] - Informações da Conta");
+							System.out.println("[5] - Sair da Conta");
+							System.out.println("-----------------------------");
+							System.out.print("Escolha sua opção: ");
+							opcao = sc.nextInt(); //ler a opçao digitada
+							
+							switch (opcao) {
+								case 1:
+									System.out.println("\nSaldo disponível: R$" + contaLogada1.getSaldo());
+									System.out.println("----------------------------");
+									System.out.print("Digite o valor para depósito: R$");
+									double valorDep = sc.nextDouble();
+									contaLogada1.depositar(valorDep);
+									break;
+									
+								case 2:
+									System.out.println("\nSaldo disponível: R$" + contaLogada1.getSaldo());
+									System.out.println("----------------------------");
+									System.out.print("Digite o valor para saque: R$");
+									double valorSaq = sc.nextDouble();
+									contaLogada1.sacar(valorSaq);
+									break;
+									
+								case 3: 
+									contaLogada1.exibirExtrato();
+									break;
+									
+								case 4:
+									contaLogada1.exibirDados();
+									break;
+									
+								case 5:
+									System.out.println("Sainda da Conta...Retornando ao Menu Principal.");
+									System.out.println("Sessão finalizada, até logo...");
+									break;
+								
+								default:
+									System.out.println("Opção Invalida");
+									}	
+								} 
+                    }else {
+                    	System.out.println("\n[ERRO] Nenhuma conta encontrada com o CPF: " + buscaCpf);
+                    }
+                    break;
+                    
+				case 3:
+                    System.out.println("\nEncerrando o James Bank. Volte sempre!");
+                    break;
+
+                default:
+                    System.out.println("\n [ERRO] Opção inválida!");
+			}        
 		}
 	sc.close();
 	
